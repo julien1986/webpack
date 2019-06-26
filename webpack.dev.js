47c7f5ce8e5b140f3412ee6792411973d52a3236
ingrwf <ingrwf@cpg-453069.cepegra2008.lan>
2019-06-26 16:31:00 +0200

    ajout footer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,35 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const htmlLoader = require('html-loader');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlLoader = require("html-loader");
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/index.js',
+    devtool: "eval-cheap-module-source-map",
+    entry: "./src/index.js",
     devServer: {
         port: 8080,
         contentBase: path.join(__dirname, "dist")
     },
     node: {
-        fs: 'empty'
+        fs: "empty"
     },
     module: {
         rules: [
             {
                 test: /\.(html)$/,
                 use: {
-                    loader: 'html-loader',
+                    loader: "html-loader",
                     options: {
-                        attrs [':data-src']
+                        minimise: true
                     }
                 }
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 options: {
-                    presets: ['env']
+                    presets: ["env"]
                 }
             },
             {
@@ -53,44 +53,42 @@ module.exports = {
                         // compiles Sass to CSS
                         loader: "sass-loader",
                         options: {
-                            outputStyle: 'expanded',
+                            outputStyle: "expanded",
                             sourceMap: true,
                             sourceMapContents: true
                         }
                     }
                     // Please note we are not running postcss here
                 ]
-            }
-            ,
+            },
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
-                        loader: 'url-loader',
+                        loader: "url-loader",
                         options: {
                             // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
+                            name: "[path][name].[ext]?hash=[hash:20]",
                             limit: 8192
                         }
                     }
                 ]
-            }
-            ,
+            },
             {
                 // Load all icons
                 test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: "file-loader"
                     }
                 ]
             }
-        ],
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
+            template: "./index.html",
             inject: true
         })
     ]
