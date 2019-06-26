32a3bb75c73ff3a847cc38e3a9d7185d911b1ecc
ingrwf <ingrwf@cpg-453069.cepegra2008.lan>
2019-06-26 13:47:35 +0200

    add axios

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+require("normalize.css/normalize.css");
+require("./styles/index.scss");
 
-require('normalize.css/normalize.css');
-require('./styles/index.scss');
+import Axios from "axios";
+let url = "https://opendata.visitflanders.org/tourist/brands/beer_styles.json?page=1&page_size=500&limit=1&fbclid=IwAR1hij-RkMGJ1TsBkqpeTsNh_wCQIZ0W3uL8HrRO8qlRYmqGoe8SqDuu5iI";
+
+Axios.get(url)
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(console.log("eror"));
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    const pluginsTriggerElement = document.getElementById('plugins-trigger');
-    const pluginsElement = document.getElementById('plugins');
+    const pluginsTriggerElement = document.getElementById("plugins-trigger");
+    const pluginsElement = document.getElementById("plugins");
 
     const pluginsVisibleClass = "splash-overview-plugins__list--visible";
 
     pluginsTriggerElement.onclick = () => {
         pluginsElement.classList.toggle(pluginsVisibleClass);
-    }
+    };
 });
